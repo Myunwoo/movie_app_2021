26c5b5482d26ac5d51ea28613f198f08938a44f4
Myunwoo <63415602+Myunwoo@users.noreply.github.com>
2021-05-08 17:30:06 +0900

    20210508 study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import Proptypes from "prop-types";
 
-function Food({fav}){
-  return(
-    <h1>I like {fav}</h1>
-  );
-}
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
 
-const foodILike = [];
+  getMovies = async() => {
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false})
+  }
 
-function App() {
-  return (
-    <div className="App">
-      Hello!
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="beer" />
-      <Food fav="chukumi" />
-    </div>
-  );
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div> 
+        ) : (  
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key = {movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} 
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
