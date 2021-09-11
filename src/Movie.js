22645e551c84b338bb2876fe9ecf2c4ac408552d
Myunwoo <63415602+Myunwoo@users.noreply.github.com>
2021-09-12 06:18:30 +0900

    123

--- src/Movie.js
+++ src/Movie.js
@@ -5,14 +5,18 @@ import './Movie.css';
 
 function Movie({year, title, summary, poster, genres}){
     return <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-            {genres.map((genre,index) => <li key={index} className="movie__genres__genre">
+        <div className="movie__column">
+            <img src={poster} alt={title} title={title} />
+        </div>
+        <div className="movie__column">
+            <span className="movie__title">{title}</span>
+            <span className="movie__year">{year}</span>
+            <ul className="movie__genres">
+                {genres.map((genre,index) => <li key={index} className="movie__genres__genre">
                 {genre}</li>)}
-        </ul>
-        <p className="movie__summary">{summary}</p>
+            </ul>
+        <span className="movie__summary">{summary}</span>
+        </div>
     </div>;
 }
 
